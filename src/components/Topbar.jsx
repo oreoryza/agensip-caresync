710d5fd8bfce2fbb3b2a0c77b519918ffee65787
oreoryza <moryzasativa@gmail.com>
2025-10-09 12:03:08 +0700

    fix images doesnt appear

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleToken } from "../redux/slices/tokenSlice";
 
+import logo from "../assets/logo.svg"
+
 import { RiSearchLine } from "react-icons/ri";
 import { RiSettings3Line } from "react-icons/ri";
 import { RiNotification3Line } from "react-icons/ri";
@@ -14,28 +16,14 @@ const Topbar = () => {
   const dispatch = useDispatch();
 
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
 
   const handleCollapsed = () => {
-    if (isCollapsed) {
-      // closing
-      setIsCollapsed(false);
-    } else {
-      // opening
-      setIsVisible(true);
-      setIsCollapsed(true);
-    }
-  }
-
-  const handleAnimationEnd = () => {
-    if (!isCollapsed) {
-      setIsVisible(false);
-    }
+      setIsCollapsed(!isCollapsed);
   }
 
   return (
     <div className="flex justify-between items-center w-full py-[24px] px-[40px] sticky top-0 z-2 backdrop-blur-lg">
-      <img src="src/assets/logo.svg" alt="CareSync" className="max-h-[32px]" />
+      <img src={logo} alt="CareSync" className="max-h-[32px]" />
       <div className="flex items-center gap-[10px] bg-white py-[10px] pl-[12px] pr-[16px] rounded-full overflow-hidden">
         <RiSearchLine className="size-[23px]" />
         <input type="text" placeholder="Search" className="focus:outline-0" />
@@ -47,7 +35,7 @@ const Topbar = () => {
         <div className="group h-[42px] w-[42px] flex justify-center items-center bg-white rounded-[100%]">
           <RiNotification3Line className="size-[20px] group-hover:rotate-30 duration-500" />
         </div>
-        <div onClick={handleCollapsed} className="group flex items-center bg-white p-[5px] rounded-full cursor-pointer">
+        <div onClick={handleCollapsed} className="group flex items-center bg-white p-[5px] rounded-full cursor-pointer z-1">
           <div className="bg-green size-[34px] rounded-[100%] mr-[7px]"></div>
           <p>Guy Hawkins</p>
           {isCollapsed ? (<FaChevronUp className="ml-[12px] mr-[6px] p-[2px] group-hover:-translate-y-0.5 duration-500" />):(<FaChevronDown className="ml-[12px] mr-[6px] p-[2px] group-hover:translate-y-0.5 duration-500" />)}
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Login from "../sections/Login";
 import SignUp from "../sections/SignUp";
 
+import logo from "../assets/logo.svg"
+import loginImg from "../assets/login-img.jpg"
+import signupImg from "../assets/signup-img.jpg"
+
 import { RiShining2Fill } from 'react-icons/ri';
 
 const Auth = () => {
@@ -13,7 +17,7 @@ const Auth = () => {
     <div className="flex justify-between p-[20px] w-screen h-screen">
       <div className="flex flex-col items-center gap-[48px] py-8 w-[46%] h-full">
         <img
-          src="src/assets/logo.svg"
+          src={logo}
           alt="CareSync"
           className="max-w-[208px]"
         />
@@ -24,11 +28,10 @@ const Auth = () => {
         )}
       </div>
       <div
-        className={`w-[54%] h-full ${
-          isLogin ? "bg-[url(src/assets/login-img.jpg)]" : "bg-[url(src/assets/signup-img.jpg)]"
-        } bg-cover rounded-[20px] overflow-hidden`}
+        className="relative w-[54%] h-full bg-cover rounded-[20px] overflow-hidden"
       >
-        <div className="flex flex-col justify-end gap-4 w-full h-full px-[36px] py-[32px] text-white backdrop-brightness-50">
+        <img src={isLogin ? loginImg : signupImg} alt="Doctors" className="object-cover h-full w-full" />
+        <div className="absolute bottom-0 flex flex-col justify-end gap-4 w-full h-full px-[36px] py-[32px] text-white backdrop-brightness-50">
             <div><RiShining2Fill className="size-[45px]"/></div>
           <h1>Effortless Care</h1>
           <p>
@@ -43,6 +46,7 @@ const Auth = () => {
             <div className="bg-green size-[52px] rounded-[100%] outline-2 outline-white/[.6] -ml-3"></div>
           </div>
         </div>
+        
       </div>
     </div>
   );
