f9e6270de67da44288d580d134e15598c466cc52
oreoryza <moryzasativa@gmail.com>
2025-10-09 11:37:58 +0700

    fix wrong page name

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import {store, persistor} from './Redux/index.js'
+import {store, persistor} from './redux/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
